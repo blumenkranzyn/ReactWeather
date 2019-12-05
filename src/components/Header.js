6f192ba7b585b639d8c33596f3dca03bd4713fc4
Amanda Natallie <amanda.natallie.2@gmail.com>
2019-12-05 16:20:44 -0300

    Intro 2

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,16 @@
 import React from "react"
-import { Container, Row } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 
 
 const Header = () => {
     return (
 
         <section className="header flex items-center">
-
             <Container>
                 <Row>
-                    <div class="ml-6 pt-1">
-                        <h4 class="text-xl text-white-900 leading-tight"> <i className="fas fa-map-pin"></i> São Paulo/SP</h4>
-                        <p class="text-base text-white-600 leading-normal">Confira o clima em sua cidade!</p>
-                    </div>
+                    <Col>
+                        
+                    </Col>
                 </Row>
             </Container>
         </section>
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -2,6 +2,7 @@ import React, { Fragment } from "react"
 import { Flip } from 'react-reveal';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAngleDown } from '@fortawesome/free-solid-svg-icons'
+import { Container, Row, Col } from 'react-bootstrap';
 
 const Intro = () => {
 
@@ -12,28 +13,38 @@ const Intro = () => {
 
     return (
         <Fragment>
-
-            
             <div className="intro">
-                <Flip bottom>
-                    <h1>Seja Bem Vindo!</h1>
-                </Flip>
-                <Flip delay={600} bottom>
-                    <h3>A Previsão do Tempo</h3>
-                </Flip>
-                <Flip delay={1300} bottom>
-                    <h5>Da sua cidade você encontra aqui!</h5>
-                </Flip>
+                <div class="ocean">
+                    <div class="wave"></div>
+                    <div class="wave"></div>
+                    <div class="wave"></div>
+                </div>
+                <Container>
+                    <Row>
+                        <Col xs={4}>
+                            <Flip bottom>
+                                <img src="assets/icon-intro.png" alt="Imagem de Introdução" />
+                            </Flip>
+                        </Col>
+                        <Col xs={8}>
+                            <Flip delay={200} bottom>
+                                <h1>Seja Bem Vindo ao</h1>
+                            </Flip>
+                            <Flip delay={500} bottom>
+                                <h3>climaTempo</h3>
+                            </Flip>
+                            <Flip delay={1000} bottom>
+                                <h5>Veja o clima da sua cidade aqui!</h5>
+                            </Flip>
+                        </Col>
+                    </Row>
+                </Container>
 
 
                 <button onClick={(e) => handleClick(e)} className="bounce" >
                     <FontAwesomeIcon icon={faAngleDown} />
                 </button>
-                <div class="ocean">
-                <div class="wave"></div>
-                <div class="wave"></div>
-                <div class="wave"></div>
-            </div>
+
             </div>
         </Fragment>
     )
