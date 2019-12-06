337c54d48121ba20124589a0e2a598784ecb7f14
Amanda Natallie <amanda.natallie.2@gmail.com>
2019-12-06 14:13:31 -0300

    Building PROD 1

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -7,7 +7,7 @@ const Dashboard = () => {
     const [data, setData] = useState(null)
 
     const getData = async () => {
-        let res = await axios.get("http://apiadvisor.climatempo.com.br/api/v1/forecast/locale/3477/days/15?token=356e147e0a87fa946bafdaaea65137ae");
+        let res = await axios.get("https://apiadvisor.climatempo.com.br/api/v1/forecast/locale/3477/days/15?token=356e147e0a87fa946bafdaaea65137ae");
         let { data } = res.data;
         setData(data);
     };
