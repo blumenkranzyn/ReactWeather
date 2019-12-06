f3bab9ddf48becfefc1d73d8df587c6e37cebccf
Amanda Natallie <amanda.natallie.2@gmail.com>
2019-12-06 14:04:11 -0300

    Building PROD

---
+++ build/precache-manifest.a6414478047c8e9fce2c6e3d87c68142.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a8633260cb65f512c83b715a9a3fc7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "633012bcd83b17ef554d",
+    "url": "/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "34ba5fb514977b18012d",
+    "url": "/static/css/main.3de9824c.chunk.css"
+  },
+  {
+    "revision": "633012bcd83b17ef554d",
+    "url": "/static/js/2.b2d1f06b.chunk.js"
+  },
+  {
+    "revision": "34ba5fb514977b18012d",
+    "url": "/static/js/main.1959a11f.chunk.js"
+  },
+  {
+    "revision": "f6ab0ab0ba1f5dff3d48",
+    "url": "/static/js/runtime-main.990a2560.js"
+  },
+  {
+    "revision": "e619787e73ffb15d48fc4569884ec039",
+    "url": "/static/media/bg-intro.e619787e.jpg"
+  },
+  {
+    "revision": "c2688c14286d8499d458684ab5d65a5e",
+    "url": "/static/media/bg-weather.c2688c14.jpg"
+  }
+]);
